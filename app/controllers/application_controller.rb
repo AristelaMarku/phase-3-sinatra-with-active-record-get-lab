eb6c2a44a109475f78e67746ee635cb814e25362
Aristela Marku <aristela.marku@gmail.com>
2021-11-12 14:42:36 +0100

    commit tree bbd842952eb018aa4a6b7bdb697d9042d273943d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < Sinatra::Base
 
   # add routes
+  set :default_content_type, 'application/json'
+  
+  get "/bakeries" do
+   
+    bakeries=Bakery.all
+
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery=Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+  end
 
 end
